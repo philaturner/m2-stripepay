f64f772ad853e47f9c34648e6c4a1c55561edb06
Phil Turner <julietius@gmail.com>
2018-05-12 21:11:14 +0100

    mvp checkout component

--- view/frontend/web/js/view/payment/method-renderer/stripepay.js
+++ view/frontend/web/js/view/payment/method-renderer/stripepay.js
@@ -2,9 +2,15 @@ define(
     [
         'Magento_Checkout/js/view/payment/default',
         'Magento_Checkout/js/model/quote',
+        'jquery',
+        'Magento_Checkout/js/model/full-screen-loader',
+        'Magento_Payment/js/model/credit-card-validation/validator',
+        'Magento_Checkout/js/model/payment/additional-validators',
+        'Magento_Checkout/js/action/place-order',
+        'Magento_Checkout/js/action/redirect-on-success',
         'https://js.stripe.com/v3/'
     ],
-    function (Component, quote) {
+    function (Component, quote, $, screenLoader, validator, additionalValidators, placeOrderAction, redirectOnSuccess){
         'use strict';
 
         return Component.extend({
@@ -12,23 +18,65 @@ define(
                 template: 'PhilTurner_StripePay/payment/stripepay',
                 stripe: null,
                 stripeCardElement: null,
-                stripeCard: null
+                stripeCard: null,
+                stripeToken: null
             },
 
-            placeOrder: function(data, event) {
+            placeOrder: function(data, event){
+                let self = this;
+                let defer;
+
+                if (event) event.preventDefault();
+
+
+                //disable button and start loader
+                this.isPlaceOrderActionAllowed(false);
+                screenLoader.startLoader();
+
+                //get billing info from quote
                 let stripeAddressData = this.getBillingData();
-                console.log(stripeAddressData);
-                return false;
+
+                //try get stripe token
+                defer = this.setStripeToken(stripeAddressData);
+
+                $.when(defer).done(function(result) {
+                    //TODO remove logging
+                    console.log(self.stripeToken);
+                    self.actionOrder();
+                }).fail(function(result) {
+                    screenLoader.stopLoader();
+                    self.isPlaceOrderActionAllowed(true);
+                    self.messageContainer.addErrorMessage({
+                        'message': result
+                    });
+                });
+                return true;
             },
 
-            initialize: function() {
+            actionOrder: function() {
+                let self = this;
+                let placeOrder = placeOrderAction(self.getData(), self.messageContainer);
+
+                $.when(placeOrder).done(function() {
+                    if (self.redirectAfterPlaceOrder) {
+                        redirectOnSuccess.execute();
+                    }
+                }).fail(function(response) {
+                    //TODO remove logging
+                    console.log(response.error);
+                    screenLoader.stopLoader();
+                    self.isPlaceOrderActionAllowed(true);
+                });
+            },
+
+            initialize: function(){
                 this._super();
                 //TODO use get method to get key from config
                 this.stripe = Stripe('pk_test_LhdtmRoQafJFa6LeAq19Bt1P');
             },
 
             initStripe: function() {
-                var self = this;
+                let self = this;
 
                 let style = {
                     base: {
@@ -52,17 +100,25 @@ define(
                 self.stripeCard = self.stripeCardElement.create('card', { style: style });
                 self.stripeCard.mount('#stripe-card');
 
-                // self.stripeCard.addEventListener('change', function(event) {
-                //     var displayError = document.getElementById('card-errors');
-                //     if (event.error) {
-                //         displayError.textContent = event.error.message;
-                //     } else {
-                //         displayError.textContent = '';
-                //     }
-                // });
             },
 
-            getBillingData: function () {
+            setStripeToken: function(customerAddress){
+                let self = this;
+                let defer = $.Deferred();
+
+                self.stripe.createToken(self.stripeCard, customerAddress).then(function(result) {
+                    if (result.error) {
+                        defer.reject(result.error.message);
+                    }else {
+                        self.stripeToken = result.token;
+                        defer.resolve({});
+                    }
+                });
+
+                return defer.promise()
+            },
+
+            getBillingData: function(){
                 let billingAddress = quote.billingAddress();
 
                 let stripeAddressData = {
@@ -76,6 +132,10 @@ define(
                 if (billingAddress.hasOwnProperty('regionCode')) stripeAddressData.address_state = billingAddress.regionCode;
                 if (!stripeAddressData.address_state) stripeAddressData.address_state = '';
                 return stripeAddressData;
+            },
+
+            getCode: function(){
+                return 'stripepay';
             }
         });
     }
